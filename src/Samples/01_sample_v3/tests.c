414d939743eb2178739f4467b55166cd0c841785
kazumichi kuroda <kazumichi96@gmail.com>
2021-04-15 02:41:17 +0900

    Added 01_sample_v3

---
+++ src/Samples/01_sample_v3/tests.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include "tests.h"
+
+void test1(void) {
+  printf("Test1の処理です\n");
+}
+
+void test2(void) {
+  printf("Test2の処理です\n");
+}
+
+void test3(void) {
+  printf("Test3の処理です\n");
+}
